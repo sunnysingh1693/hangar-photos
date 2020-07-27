3f0453be9d61dba7e6f02b766c91f1e80d7cb5bd
Sunny Singh <sunnysingh@MacBook-Pro.local>
2020-07-28 02:55:15 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hangar-photos/precache-manifest.a58233ffae17073934dc1c2f1c2a5b57.js"
+  "/hangar-photos/precache-manifest.632673f0583e6cb7c4e16f2d390b6f4c.js"
 );
 
 self.addEventListener('message', (event) => {
