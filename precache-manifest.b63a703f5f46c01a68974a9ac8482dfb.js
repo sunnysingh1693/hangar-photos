a254b5d15ecbcdac853b0530d39a8276f12b4b26
Sunny Singh <sunnysingh@MacBook-Pro.local>
2020-07-28 02:31:15 +0530

    Updates

---
+++ precache-manifest.b63a703f5f46c01a68974a9ac8482dfb.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ed976962065ab50613feca1329d18f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "caa5110c6058e41d04f2",
+    "url": "./static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8b260d41b7d9a9f1b122",
+    "url": "./static/css/main.16601232.chunk.css"
+  },
+  {
+    "revision": "caa5110c6058e41d04f2",
+    "url": "./static/js/2.6fd63561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6fd63561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b260d41b7d9a9f1b122",
+    "url": "./static/js/main.0478ecf2.chunk.js"
+  },
+  {
+    "revision": "c4435cd4b27a3110f448",
+    "url": "./static/js/runtime-main.8b64b413.js"
+  },
+  {
+    "revision": "1cc987cbe1dd320885df26c6a8632f43",
+    "url": "./static/media/Aileron-Bold-webfont.1cc987cb.ttf"
+  },
+  {
+    "revision": "317ed94a878c8d8ea413f51e575513f4",
+    "url": "./static/media/Aileron-Bold-webfont.317ed94a.woff"
+  },
+  {
+    "revision": "3cdf602b3281ee10ef3c7260cafd634f",
+    "url": "./static/media/Aileron-Bold-webfont.3cdf602b.otf"
+  },
+  {
+    "revision": "9df23ffb1fc0a629005df32644a24b70",
+    "url": "./static/media/Aileron-Bold-webfont.9df23ffb.woff2"
+  },
+  {
+    "revision": "bccd23a84e30eb286d748dc86b09c49d",
+    "url": "./static/media/Aileron-Bold-webfont.bccd23a8.eot"
+  },
+  {
+    "revision": "deb29bc75156be488415ac59f333d71c",
+    "url": "./static/media/Aileron-Bold-webfont.deb29bc7.svg"
+  },
+  {
+    "revision": "621ba5aaf66e52f3def0cf69807104b9",
+    "url": "./static/media/Aileron-Heavy-webfont.621ba5aa.otf"
+  },
+  {
+    "revision": "6dab36a9692b36d02984ee95716b7ef7",
+    "url": "./static/media/Aileron-Heavy-webfont.6dab36a9.woff2"
+  },
+  {
+    "revision": "977a8badf138ba0183b618103fbea86a",
+    "url": "./static/media/Aileron-Heavy-webfont.977a8bad.woff"
+  },
+  {
+    "revision": "a765488db4d152dad4968f4faf02c126",
+    "url": "./static/media/Aileron-Heavy-webfont.a765488d.ttf"
+  },
+  {
+    "revision": "cbca114bace1dcafcba0ccf9b91c56f4",
+    "url": "./static/media/Aileron-Heavy-webfont.cbca114b.eot"
+  },
+  {
+    "revision": "fa87079ec53550641957a5b3725802f9",
+    "url": "./static/media/Aileron-Heavy-webfont.fa87079e.svg"
+  },
+  {
+    "revision": "20b648f197c843b978ab18535d7b2b4b",
+    "url": "./static/media/Aileron-Light-webfont.20b648f1.woff2"
+  },
+  {
+    "revision": "35ede60c5aa47c2262853af06ed0c28c",
+    "url": "./static/media/Aileron-Light-webfont.35ede60c.eot"
+  },
+  {
+    "revision": "7120f405f94a79633cb8671daf076264",
+    "url": "./static/media/Aileron-Light-webfont.7120f405.otf"
+  },
+  {
+    "revision": "c8c8da05d4a5bea206f0a9da69f2963a",
+    "url": "./static/media/Aileron-Light-webfont.c8c8da05.ttf"
+  },
+  {
+    "revision": "da75003ce72fcafb82668aa8d9eb1bba",
+    "url": "./static/media/Aileron-Light-webfont.da75003c.woff"
+  },
+  {
+    "revision": "f095fbfe098e724b2964b8a31587f45a",
+    "url": "./static/media/Aileron-Light-webfont.f095fbfe.svg"
+  },
+  {
+    "revision": "4309f5e6504ab4404a1c909a5ef8457f",
+    "url": "./static/media/Aileron-Regular-webfont.4309f5e6.woff"
+  },
+  {
+    "revision": "74bbbab2c7f147a278ebc9866b5394de",
+    "url": "./static/media/Aileron-Regular-webfont.74bbbab2.svg"
+  },
+  {
+    "revision": "9f1d7341a18b9de6061ecdf4a9adcc72",
+    "url": "./static/media/Aileron-Regular-webfont.9f1d7341.eot"
+  },
+  {
+    "revision": "c9b9f1d8f6826c2683987a76e73adea6",
+    "url": "./static/media/Aileron-Regular-webfont.c9b9f1d8.woff2"
+  },
+  {
+    "revision": "d321fa78bb7190a8ca7e14213ef63203",
+    "url": "./static/media/Aileron-Regular-webfont.d321fa78.otf"
+  },
+  {
+    "revision": "e0fde64e04cb74353bdd55f6af7bd6f7",
+    "url": "./static/media/Aileron-Regular-webfont.e0fde64e.ttf"
+  },
+  {
+    "revision": "bd3df00fa1a93b281cb22986f03c09b1",
+    "url": "./static/media/arrow.bd3df00f.svg"
+  },
+  {
+    "revision": "29cc016cecb1a6f26c02b8e4cf7eb034",
+    "url": "./static/media/bg-home.29cc016c.jpg"
+  },
+  {
+    "revision": "ecbf8d23bdf8f2c61dda18b3e7c3de6c",
+    "url": "./static/media/bg-start.ecbf8d23.jpg"
+  },
+  {
+    "revision": "5f1cfe60abca790df8adffbca27fb9aa",
+    "url": "./static/media/hangar_logo_large.5f1cfe60.svg"
+  },
+  {
+    "revision": "95143775b529d8c842032beaa0dd29d7",
+    "url": "./static/media/hangar_logo_small.95143775.svg"
+  },
+  {
+    "revision": "2d58e4a6b0aff1d345b3892329c3cc99",
+    "url": "./static/media/icons8-facebook-f.2d58e4a6.svg"
+  },
+  {
+    "revision": "1ab390b7f86cdd2c0d184375d463837b",
+    "url": "./static/media/icons8-linkedin-2.1ab390b7.svg"
+  },
+  {
+    "revision": "c042dbb0bc1091ef42362b6f429efb12",
+    "url": "./static/media/noresults.c042dbb0.png"
+  }
+]);
